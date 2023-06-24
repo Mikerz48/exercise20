b65bcdbb202e19dea3f8c6533422a39fc3c12adc
Mikerz48 <michaelrenna48@gmail.com>
2023-06-24 17:30:50 +0200

    Done

---
+++ exercise.js
@@ -0,0 +1,42 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+    static calculate (geometricShape) {
+      switch (true) {
+        case geometricShape instanceof Square:
+          return Math.pow(geometricShape.side, 2);
+
+        case geometricShape instanceof Rectangle:
+          return (geometricShape.width * geometricShape.height);
+
+        case geometricShape instanceof Circle:
+          return Math.PI * Math.pow(geometricShape.radius, 2);
+      }
+  }
+}
+
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
